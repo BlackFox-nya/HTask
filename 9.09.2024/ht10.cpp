11d6c661fb4f110ce6f6e2a3f6b675c993dd9298
Mykola Andriushkin <m.a.andriushkin@student.khai.edu>
2024-09-13 14:51:56 +0300

    9/09/2024

--- 9.09.2024/ht10.cpp
+++ 9.09.2024/ht10.cpp
@@ -2,15 +2,16 @@
 using namespace std;
 
 int main () {
-	double r1, r2, s1, s2, s3, pi;
-	pi = 3.14;
-	cout << "Enter R1 - ";
+	// Початок завдання
+	double r1, r2, s1, s2, s3, pi; // Декларація змінних
+	pi = 3.14; // Введення пі
+	cout << "Enter R1 - "; // Введення R1
 	cin >> r1;
-	cout << "Enter R2 - ";
+	cout << "Enter R2 - "; // Введення R2
 	cin >> r2;
-	s1 = pi * (r1 * r1);
+	s1 = pi * (r1 * r1); // Розрахунок
 	s2 = pi * (r2 * r2);
 	s3 = s1 - s2;
-	cout << "S1 - " << s1 << endl << "S2 - " << s2 << endl << "S3 - " << s3 << endl;
+	cout << "S1 - " << s1 << endl << "S2 - " << s2 << endl << "S3 - " << s3 << endl; // Вивід результату
 	return 0;
 }
--- 9.09.2024/ht2.cpp
+++ 9.09.2024/ht2.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 using namespace std;
 int main () {
-	double a, c;
-	cout << "Enter a: ";
+        // Початок задачі
+	double a, c; // Декларація змінних
+	cout << "Enter a: "; // Введення значення змінної
 	cin >> a;
-	c = a * a;
-	cout << "Area of the square - " << c << "\n";
+	c = a * a; // Розрахунок
+	cout << "Area of the square - " << c << "\n"; // Вивід результату
 	return 0;
 }
