f31e3a9b99fd46d6b6e3c08a813a1e2408daf54b
Mykola Andriushkin <m.a.andriushkin@student.khai.edu>
2024-09-13 14:21:29 +0300

    9/09/2024

--- 9.09.2024/ht2.cpp
+++ 9.09.2024/ht2.cpp
@@ -1,16 +1,10 @@
 #include <iostream>
 using namespace std;
-
 int main () {
-	double r1, r2, s1, s2, s3, pi;
-	pi = 3.14;
-	cout << "Enter R1 - ";
-	cin >> r1;
-	cout << "Enter R2 - ";
-	cin >> r2;
-	s1 = pi * (r1 * r1);
-	s2 = pi * (r2 * r2);
-	s3 = s1 - s2;
-	cout << "S1 - " << s1 << endl << "S2 - " << s2 << endl << "S3 - " << s3 << endl;
+	double a, c;
+	cout << "Enter a: ";
+	cin >> a;
+	c = a * a;
+	cout << "Area of the square - " << c << "\n";
 	return 0;
 }
