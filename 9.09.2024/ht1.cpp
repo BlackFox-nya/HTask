e05e3d5567a91f040825ea339c657b562fc7bc92
Mykola Andriushkin <m.a.andriushkin@student.khai.edu>
2024-09-13 13:40:33 +0300

    Yeeeeet x2

---
+++ 9.09.2024/ht1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main () {
+	int a;
+	int c;
+	cout << "Enter a: ";
+	cin >> a;
+	c = a * a;
+	cout << "Area of the square - " << c << "\n";
+	return 0;
+}
