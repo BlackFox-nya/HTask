9f3d0ee91f8e6a20bc69b1d584c3defa8b72c147
BlackFox-nya <m.a.andriushkin@student.khai.edu>
2024-12-09 13:31:00 +0200

    equ2 works in theory

--- LAB5/ht.cpp
+++ LAB5/ht.cpp
@@ -1,168 +1,128 @@
 #include <iostream>
 #include <cmath>
+#include <limits>
+
 #define _USE_MATH_DEFINES
 
 using namespace std;
 
-// Functions
+// Function declarations
 void geo36();
 bool inBrownRegion(double r, double x, double y);
 double pointDist(double ax, double ay, double bx, double by);
 double triangArea(double AB, double AC, double BC);
 void eq1();
 void eq2();
+double safeFactorial(int n);
+double computeSeries(double x, int n);
+double degToRad(double x);
+double sineTaylorSeries(double x, int terms);
 
-bool useless(int mn){
-    if(mn > 0 && mn < 5){
-        return true;
-    }
-    else{
-        return false;
-    }
-}
-
-// Function
+// Main function
 int main() {
-    cout << "Select task: \n" << "1: GEO 36 \n" << "2: Equation 1 \n" << "3: Equation 2 \n" << "4: Exit \n" << "You select - ";
-    int crs;
-    cin >> crs;
-    useless(crs);
-    switch (crs)
-    {
-    case 1: geo36(); main();
-    case 2: eq1(); main();
-    case 3: eq2(); main();
-    case 4: break;
-    default: cout << "Wrong input"; main();
+    while (true) {
+        cout << "Select task: \n"
+             << "1: GEO 36 \n"
+             << "2: Equation 1 \n"
+             << "3: Equation 2 \n"
+             << "4: Exit \n"
+             << "You select - ";
+        int crs;
+        cin >> crs;
+
+        switch (crs) {
+            case 1: geo36(); break;
+            case 2: eq1(); break;
+            case 3: eq2(); break;
+            case 4: return 0;
+            default: cout << "Wrong input. Please try again.\n"; break;
+        }
     }
     return 0;
 }
 
+// GEO 36 functionality
 void geo36() {
-    cout << "************* GEO 36 *****************" << endl;
+    cout << "************* GEO 36 *****************\n";
 
-    // Input number of points
     cout << "Enter number of points (i): ";
     int i;
     cin >> i;
 
-    // Input radius
     cout << "Enter radius (r): ";
     double rin;
     cin >> rin;
 
-    // Allocate memory for coordinates
     double* crds = new double[i * 2];
     for (int m = 0; m < i; m++) {
         cout << "Enter coordinates for point " << m + 1 << " (x y): ";
         cin >> crds[m * 2] >> crds[m * 2 + 1];
     }
 
-    int countInBrownRegion = 0; // Counter for points in the brown region
-
-    // Check each point and display results
+    int countInBrownRegion = 0;
     for (int m = 0; m < i; m++) {
         double x = crds[m * 2];
         double y = crds[m * 2 + 1];
         bool result = inBrownRegion(rin, x, y);
 
-        // Display the result for each point
-        cout << "Point (" << x << ", " << y << ") is ";
-        cout << (result ? "in the brown region." : "out of the brown region.") << endl;
+        cout << "Point (" << x << ", " << y << ") is "
+             << (result ? "in the brown region." : "out of the brown region.") << endl;
 
         if (result) {
             countInBrownRegion++;
         }
     }
 
-    // Display the number of points in the brown region
     cout << "Total number of points in the brown region: " << countInBrownRegion << endl;
 
-    delete[] crds; // Free allocated memory
+    delete[] crds;
 }
 
-// Function to calculate distance between two points
-double pointDist(double ax, double ay, double bx, double by) {
-    return sqrt(pow(bx - ax, 2.0) + pow(by - ay, 2.0));
-}
-
-// Function to calculate the area of a triangle given its sides
-double triangArea(double AB, double AC, double BC) {
-    double p = (AB + AC + BC) / 2.0; // Semi-perimeter
-    return sqrt(p * (p - AB) * (p - AC) * (p - BC)); // Heron's formula
-}
-
-// Function to check if a point is in the "brown region"
 bool inBrownRegion(double r, double x, double y) {
-    // Triangle vertices
-    double ax = 0, ay = 0;       // Vertex A
-    double bx = 0, by = r;       // Vertex B
-    double cx = r / 2.0, cy = sqrt(pow(r, 2) - pow(r / 2.0, 2)); // Vertex C
+    double ax = 0, ay = 0;
+    double bx = 0, by = r;
+    double cx = r / 2.0, cy = sqrt(pow(r, 2) - pow(r / 2.0, 2));
 
-    // Calculate original triangle area
     double distAB = pointDist(ax, ay, bx, by);
     double distAC = pointDist(ax, ay, cx, cy);
     double distBC = pointDist(bx, by, cx, cy);
     double S_org = triangArea(distAB, distAC, distBC);
 
-    // Calculate distances from the point to the vertices
     double distPA = pointDist(x, y, ax, ay);
     double distPB = pointDist(x, y, bx, by);
     double distPC = pointDist(x, y, cx, cy);
 
-    // Calculate sub-triangle areas
     double S_PAB = triangArea(distAB, distPB, distPA);
     double S_PBC = triangArea(distPB, distPC, distBC);
     double S_PCA = triangArea(distPA, distPC, distAC);
 
-    // Sum of sub-triangle areas
     double S_point = S_PAB + S_PBC + S_PCA;
 
-    // Calculate circle condition
     double rsquared = pow(r, 2);
     double leftSide = pow(x - cx, 2) + pow(y - cy, 2);
 
-    // Floating-point comparison tolerance
     const double EPSILON = 1e-6;
 
-    // Check if the point is in the triangle or circle
     if (abs(S_point - S_org) < EPSILON || (leftSide <= rsquared && x >= 0)) {
-    return true;
-}
+        return true;
+    }
     return false;
 }
 
-// Function to compute the factorial of a number
-double factorial(double n) {
-    if (n <= 1) return 1;
-    long result = 1;
-    for (int i = 2; i <= n; i++) {
-        result *= i;
-    }
-    return result;
+double pointDist(double ax, double ay, double bx, double by) {
+    return sqrt(pow(bx - ax, 2.0) + pow(by - ay, 2.0));
 }
 
-// Function to compute the sine using the Taylor series
-double sineTaylorSeries(double x, int terms) {
-    double sum = 0.0;
-
-    for (int n = 0; n < terms; ++n) {
-        double term = pow(-1, n + 1) * pow(x, 2 * n + 1) / factorial(2 * n + 1);
-        sum += term;
-    }
-
-    return sum;
+double triangArea(double AB, double AC, double BC) {
+    double p = (AB + AC + BC) / 2.0;
+    return sqrt(p * (p - AB) * (p - AC) * (p - BC));
 }
 
-double degToRad(double x) { //Degrees to radians
-    return x * (M_PI / 180.0);
-}
-
+// Equation 1 functionality
 void eq1() {
     double xin, x;
     int n;
 
-    // Get user input
     cout << "Enter the value of x: ";
     cin >> xin;
     x = degToRad(xin);
@@ -170,38 +130,71 @@ void eq1() {
     cout << "Enter the number of terms in the series: ";
     cin >> n;
 
-    // Compute sine using the Taylor series
     double result = sineTaylorSeries(x, n);
 
-    cout << "The sine of " << x << " using " << n << " terms is: " << result << std::endl;
-
+    cout << "The sine of " << xin << " using " << n << " terms is: " << result << endl;
 }
 
-// Function to compute the series
-double computeSeries(double x, int terms = 100) {
+double degToRad(double x) {
+    return x * (M_PI / 180.0);
+}
+
+double sineTaylorSeries(double x, int terms) {
     double sum = 0.0;
-    
-    for (int n = 1; n <= terms; ++n) {
-        double numerator = pow(x, n - 1) + 3 * n;
-        double denominator = factorial(2 * n - 1);
-        sum += numerator / denominator;
+
+    for (int n = 0; n < terms; ++n) {
+        double term = pow(-1, n) * pow(x, 2 * n + 1) / safeFactorial(2 * n + 1);
+        sum += term;
     }
-    
+
     return sum;
 }
 
+// Equation 2 functionality
 void eq2() {
     double x;
-    int terms;
-    
-    // Input value for x and the number of terms for approximation
+    int n = 1;
+    double e = 1E-20;
+    double g = 1E+20;
+    double sum = 0.0;
+    double result = 1.0;
+
     cout << "Enter the value of x: ";
     cin >> x;
-    cout << "Enter the number of terms to compute (higher gives more accuracy): ";
-    cin >> terms;
 
-    // Calculate and display the result
-    double result = computeSeries(x, terms);
-    cout << "The computed value of the series is: " << result << std::endl;
+    while (fabs(result) >= e && fabs(result) <= g) {
+        result = computeSeries(x, n);
 
+        if (std::isinf(result)) {
+            cout << "Overflow detected. Stopping computation.\n";
+            break;
+        }
+
+        sum += result;
+        n++;
+        if (n > 100) break;
+    }
+
+    cout << "Sum is: " << sum << endl;
+    cout << "Last element is: " << result << endl;
+}
+
+double computeSeries(double x, int n) {
+    double numerator = pow(x, n - 1) + 3 * n;
+    double denominator = safeFactorial(2 * n - 1);
+    if (isinf(denominator)) {
+        return numeric_limits<double>::infinity();
+    }
+    return numerator / denominator;
+}
+
+double safeFactorial(int n) {
+    double result = 1.0;
+    for (int i = 2; i <= n; ++i) {
+        result *= i;
+        if (result > numeric_limits<double>::max()) {
+            return numeric_limits<double>::infinity();
+        }
+    }
+    return result;
 }
