acb2e59543e3abe791e8ccf6dd2f4adb6fd68cbc
BlackFox-nya <m.a.andriushkin@student.khai.edu>
2025-02-19 23:09:07 +0200

    LAB7 matrix46 works

--- LAB7/ht.cpp
+++ LAB7/ht.cpp
@@ -1,58 +1,69 @@
 #include <iostream>
 using namespace std;
 
-const int L = 20;
-const int H = 20;
+const int MAX_SIZE = 100;
 
-void matrix46();
-void matrix83();
+// Функція для введення матриці
+void inputMatrix(int matrix[][MAX_SIZE], int M, int N) {
+    for (int i = 0; i < M; ++i) {
+        for (int j = 0; j < N; ++j) {
+            cin >> matrix[i][j];
+        }
+    }
+}
+
+// Перевірка, чи є елемент максимальним у своєму рядку
+bool isMaxInRow(int row[], int currentCol, int N) {
+    for (int j = 0; j < N; ++j) {
+        if (row[j] > row[currentCol]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Перевірка, чи є елемент мінімальним у своєму стовпці
+bool isMinInCol(int matrix[][MAX_SIZE], int currentRow, int currentCol, int M) {
+    for (int i = 0; i < M; ++i) {
+        if (matrix[i][currentCol] < matrix[currentRow][currentCol]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Пошук та виведення результатів
+void findAndPrintResults(int matrix[][MAX_SIZE], int M, int N) {
+    bool found = false;
+    for (int i = 0; i < M; ++i) {
+        for (int j = 0; j < N; ++j) {
+            if (isMaxInRow(matrix[i], j, N) && isMinInCol(matrix, i, j, M)) {
+                if (!found) {
+                    cout << "Знайдені елементи: ";
+                    found = true;
+                }
+                cout << matrix[i][j] << " ";
+            }
+        }
+    }
+    if (!found) {
+        cout << "0";
+    }
+    cout << endl;
+}
 
 int main() {
-    int lp = 1;
-    while(lp == 1){
-    cout << "Selection menu: 1 - Matrix_46; 2 - Matrix_83; 3 - Exit;" << endl << "You select - "; //Message for user
-    int crs;
-    cin >> crs;
-    switch (crs) { //Menu
-        case 1: 
-            matrix46(); 
-            break;
-        case 2:
-            matrix83(); 
-            break;
-        case 3:
-            lp = 0;
-            break;
-        default: 
-            cout << "Wrong input" << endl; 
-            break;
-    }
-}
-}
-
-void get_mtx(int arr[][H], int &rows, int &col){
-    int wh = 1;
-    while(wh = 1){
-    cout << "Enter matrix size(row col)(2 - 20): ";
-    cin >> rows >> col;
-    if(rows >= 2 && rows <= 20 && col >= 2 && col <= 20){
-        cout << "Great success!";
-        wh = 2;
-    }
-    else{
-        cout << "\n ErroR - you have entered incorrect values, please check instructions which are given you and try again. \n";
-
-    }
-}
-
-}
-
-void matrix46(){
-
-    int arr[L][H];
-    int r;
-    int c;
-    get_mtx(arr, r, c);
+    int M, N;
+    cin >> M >> N;
     
-
+    if (M > MAX_SIZE || N > MAX_SIZE) {
+        cout << "0" << endl;
+        return 0;
+    }
+    
+    int matrix[MAX_SIZE][MAX_SIZE];
+    inputMatrix(matrix, M, N);      // Введення даних
+    findAndPrintResults(matrix, M, N); // Обробка та виведення
+    
+    return 0;
 }
