d3de37d31e4326c1ef453addfda042de4d7ec63b
BlackFox-nya <m.a.andriushkin@student.khai.edu>
2025-03-26 21:51:44 +0200

    LAB8_byfile with cmake

---
+++ LAB8_byfile/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/blackfox/source/HTask/LAB8_byfile
+# Build directory: /home/blackfox/source/HTask/LAB8_byfile/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
